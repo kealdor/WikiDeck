dce3bf6f5584e38551814cd24dff67ac493f98c5
Aspallar <shadowthrone99@outlook.com>
2017-11-29 16:15:02 +0000

    Decklists colorless cards and UI changes

--- WikiDeck/FormDecklists.Designer.cs
+++ WikiDeck/FormDecklists.Designer.cs
@@ -30,13 +30,15 @@
         {
             System.Windows.Forms.Label label1;
             System.Windows.Forms.Label label2;
-            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
             this.textBoxDescription = new System.Windows.Forms.TextBox();
             this.comboBoxStrategy = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.labelStatus = new System.Windows.Forms.Label();
             this.buttonOK = new System.Windows.Forms.Button();
             this.buttonCancel = new System.Windows.Forms.Button();
+            this.checkBoxIncludeColorless = new System.Windows.Forms.CheckBox();
+            this.linkLabelColorless = new System.Windows.Forms.LinkLabel();
             label1 = new System.Windows.Forms.Label();
             label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
@@ -44,33 +46,33 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new System.Drawing.Point(13, 38);
+            label1.Location = new System.Drawing.Point(16, 38);
             label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(27, 13);
+            label1.Size = new System.Drawing.Size(35, 13);
             label1.TabIndex = 1;
-            label1.Text = "Title";
+            label1.Text = "Name";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new System.Drawing.Point(13, 88);
+            label2.Location = new System.Drawing.Point(16, 64);
             label2.Name = "label2";
             label2.Size = new System.Drawing.Size(60, 13);
             label2.TabIndex = 3;
             label2.Text = "Description";
             // 
-            // textBoxTitle
+            // textBoxName
             // 
-            this.textBoxTitle.Location = new System.Drawing.Point(12, 57);
-            this.textBoxTitle.Name = "textBoxTitle";
-            this.textBoxTitle.Size = new System.Drawing.Size(483, 20);
-            this.textBoxTitle.TabIndex = 0;
+            this.textBoxName.Location = new System.Drawing.Point(82, 35);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(356, 20);
+            this.textBoxName.TabIndex = 0;
             // 
             // textBoxDescription
             // 
-            this.textBoxDescription.Location = new System.Drawing.Point(12, 104);
+            this.textBoxDescription.Location = new System.Drawing.Point(82, 61);
             this.textBoxDescription.Name = "textBoxDescription";
-            this.textBoxDescription.Size = new System.Drawing.Size(483, 20);
+            this.textBoxDescription.Size = new System.Drawing.Size(356, 20);
             this.textBoxDescription.TabIndex = 2;
             // 
             // comboBoxStrategy
@@ -82,7 +84,7 @@
             "Control",
             "Mill/Burn",
             "Combo"});
-            this.comboBoxStrategy.Location = new System.Drawing.Point(12, 150);
+            this.comboBoxStrategy.Location = new System.Drawing.Point(82, 87);
             this.comboBoxStrategy.Name = "comboBoxStrategy";
             this.comboBoxStrategy.Size = new System.Drawing.Size(162, 21);
             this.comboBoxStrategy.TabIndex = 4;
@@ -90,7 +92,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 131);
+            this.label3.Location = new System.Drawing.Point(16, 92);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(46, 13);
             this.label3.TabIndex = 5;
@@ -107,7 +109,7 @@
             // 
             // buttonOK
             // 
-            this.buttonOK.Location = new System.Drawing.Point(338, 187);
+            this.buttonOK.Location = new System.Drawing.Point(281, 148);
             this.buttonOK.Name = "buttonOK";
             this.buttonOK.Size = new System.Drawing.Size(75, 23);
             this.buttonOK.TabIndex = 7;
@@ -118,18 +120,43 @@
             // buttonCancel
             // 
             this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Location = new System.Drawing.Point(420, 187);
+            this.buttonCancel.Location = new System.Drawing.Point(363, 148);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(75, 23);
             this.buttonCancel.TabIndex = 8;
             this.buttonCancel.Text = "Cancel";
             this.buttonCancel.UseVisualStyleBackColor = true;
             // 
+            // checkBoxIncludeColorless
+            // 
+            this.checkBoxIncludeColorless.AutoSize = true;
+            this.checkBoxIncludeColorless.Location = new System.Drawing.Point(82, 114);
+            this.checkBoxIncludeColorless.Name = "checkBoxIncludeColorless";
+            this.checkBoxIncludeColorless.Size = new System.Drawing.Size(185, 17);
+            this.checkBoxIncludeColorless.TabIndex = 9;
+            this.checkBoxIncludeColorless.Text = "Include \"Colorless\" in deck colors";
+            this.checkBoxIncludeColorless.UseVisualStyleBackColor = true;
+            // 
+            // linkLabelColorless
+            // 
+            this.linkLabelColorless.AutoSize = true;
+            this.linkLabelColorless.LinkArea = new System.Windows.Forms.LinkArea(1, 2);
+            this.linkLabelColorless.Location = new System.Drawing.Point(265, 114);
+            this.linkLabelColorless.Name = "linkLabelColorless";
+            this.linkLabelColorless.Size = new System.Drawing.Size(18, 17);
+            this.linkLabelColorless.TabIndex = 10;
+            this.linkLabelColorless.TabStop = true;
+            this.linkLabelColorless.Text = "(?)";
+            this.linkLabelColorless.UseCompatibleTextRendering = true;
+            this.linkLabelColorless.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelColorless_LinkClicked);
+            // 
             // FormDecklists
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(516, 222);
+            this.ClientSize = new System.Drawing.Size(459, 186);
+            this.Controls.Add(this.linkLabelColorless);
+            this.Controls.Add(this.checkBoxIncludeColorless);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonOK);
             this.Controls.Add(this.labelStatus);
@@ -138,7 +165,7 @@
             this.Controls.Add(label2);
             this.Controls.Add(this.textBoxDescription);
             this.Controls.Add(label1);
-            this.Controls.Add(this.textBoxTitle);
+            this.Controls.Add(this.textBoxName);
             this.Name = "FormDecklists";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Decklists Entry";
@@ -151,12 +178,14 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.TextBox textBoxName;
         private System.Windows.Forms.TextBox textBoxDescription;
         private System.Windows.Forms.ComboBox comboBoxStrategy;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label labelStatus;
         private System.Windows.Forms.Button buttonOK;
         private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.CheckBox checkBoxIncludeColorless;
+        private System.Windows.Forms.LinkLabel linkLabelColorless;
     }
 }
--- WikiDeck/FormDecklists.cs
+++ WikiDeck/FormDecklists.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Windows.Forms;
 using WikiaClientLibrary;
 
@@ -6,6 +7,8 @@ namespace WikiDeck
 {
     public partial class FormDecklists : Form
     {
+        private const string colorlessCode  = "{{C}}";
+
         private WikiaClient _client;
         private Decklists _decklists;
         private Deck _deck;
@@ -21,6 +24,10 @@ namespace WikiDeck
             _cards = cards;
             _userName = userName;
             _decklists = new Decklists(_client, decklistsPageTitle);
+
+            textBoxName.TextChanged += InputTextChanged;
+            textBoxDescription.TextChanged += InputTextChanged;
+            comboBoxStrategy.TextChanged += InputTextChanged;
         }
 
         private void FormDecklists_FormClosing(object sender, FormClosingEventArgs e)
@@ -30,39 +37,78 @@ namespace WikiDeck
 
         private async void FormDecklists_Shown(object sender, EventArgs e)
         {
+            UpdateUI(true);
             _entry = await _decklists.GetEntryAsync(_deck.DeckName);
             if (_entry == null)
             {
                 labelStatus.Text = "This deck does not have an entry in decklists. It will be created.";
                 _entry = new DecklistsEntry();
                 _entry.Link = _deck.DeckName;
-                textBoxTitle.Text = _deck.DeckName;
+                textBoxName.Text = _deck.DeckName;
             }
             else
             {
-                labelStatus.Text = "This deck already has an entry in decklists. It will be updated";
-                textBoxTitle.Text = _entry.Name;
+                labelStatus.Text = "This deck already has an entry in decklists. It will be updated.";
+                textBoxName.Text = _entry.Name;
                 textBoxDescription.Text = _entry.Description;
                 comboBoxStrategy.Text = _entry.Strategy;
+                checkBoxIncludeColorless.Checked = _entry.Colors.IndexOf(colorlessCode) != -1;
             }
+            UpdateUI(false);
         }
 
         private async void buttonOK_Click(object sender, EventArgs e)
         {
             labelStatus.Text = "Updating Decklists....";
-            _entry.Name = textBoxTitle.Text;
+            _entry.Name = textBoxName.Text;
             _entry.Strategy = comboBoxStrategy.Text;
             _entry.Description = textBoxDescription.Text;
             _entry.SetSets(_deck.GetSets(_cards));
             _entry.SetColors(_deck.GetColors(_cards));
+            if (checkBoxIncludeColorless.Checked)
+            {
+                _entry.Colors += colorlessCode;
+            }
             if (string.IsNullOrEmpty(_entry.Author))
             {
                 _entry.Author = "[[User:" + _userName + "|" + _userName + "]]";
             }
             // TODO: error handling in buttonOK_Click
+            UpdateUI(true);
             await _decklists.Update(_entry);
+            UpdateUI(false);
             DialogResult = DialogResult.OK;
             Close();
+        }
+
+        private void linkLabelColorless_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string msg = "The available card data does not contain enough information to determine if a card is genuinely colorless, ";
+            msg += "so WikiDeck cannot to automatically add \"Colorles\" to the deck colors. ";
+            msg += "Check this box if the deck contains genuinely colorless (not artifact) cards.";
+            FormMessage dlg = new FormMessage(msg);
+            dlg.ShowDialog(this);
+        }
+
+        private void InputTextChanged(object sender, EventArgs e)
+        {
+            UpdateUI(false);
+        }
+
+        private void UpdateUI(bool working)
+        {
+            textBoxName.Enabled = !working;
+            textBoxDescription.Enabled = !working;
+            comboBoxStrategy.Enabled = !working;
+            checkBoxIncludeColorless.Enabled = !working;
+            buttonOK.Enabled = !working;
+            if (!working)
+            {
+                buttonOK.Enabled =
+                    textBoxName.Text != "" &&
+                    textBoxDescription.Text != "" &&
+                    comboBoxStrategy.Text != "";
+            }
         }
     }
 }
